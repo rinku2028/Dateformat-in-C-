ef98c9b80514d32f49e661b3bc519d1e3a6e4dc8
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-21 23:28:38 +0545

    Add files via upload

---
+++ validatedateintextbox.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace datevalidatetextbox
+{
+    public partial class validatedateintextbox : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Regex regex = new Regex(@"(((0|1)[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/((19|20)\d\d))$");
+
+            //Verify whether date entered in dd/MM/yyyy format.
+            bool isValid = regex.IsMatch(txtDate.Text.Trim());
+
+            //Verify whether entered date is Valid date.
+            DateTime dt;
+            isValid = DateTime.TryParseExact(txtDate.Text, "dd/MM/yyyy", new CultureInfo("en-GB"), DateTimeStyles.None, out dt);
+            if (!isValid)
+            {
+                lblMessage.Text = "Please put correct date format dd/MM/yyyy";
+            }
+            else
+            {
+                lblMessage.Text = "Date is in correct format";
+            }
+        }
+    }
+}
