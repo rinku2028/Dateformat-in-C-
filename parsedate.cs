a7431ad80ba7a18e8662d8171f20f38a94defae5
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-23 00:02:58 +0545

    Add files via upload

---
+++ parsedate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace dateconvert
+{
+    class parsedate
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo MyCultureInfo = new CultureInfo("en-US");
+            string[] str = { " Wednesday, Jan 22, 2020", "Wednesday, Jan 22, 2020" };
+            foreach (string dateString in str)
+            {
+                try
+                {
+                    DateTime d1 = DateTime.ParseExact(dateString, "D", MyCultureInfo);
+                    Console.WriteLine(d1);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Not able to parse '{0}'", dateString);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
