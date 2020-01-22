1625ed2d6c111f5de302844eb9ef7592aa5bf53e
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-22 23:52:32 +0545

    Add files via upload

---
+++ stringtodate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace dateconvert
+{
+    class stringtodate
+    {
+        static void Main(string[] args)
+        {
+         
+            string str = "05/01/2020 09:34:00";
+            DateTime date1;
+            if (DateTime.TryParse(str, out date1))
+            {
+                Console.WriteLine("Convert to Date: '{0}' to {1}.", str, date1);
+            }
+            else
+            {
+                Console.WriteLine("Not convert  '{0}' to a date.", str);
+            }
+            Console.ReadLine();
+        }
+    }
+}
