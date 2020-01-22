5c493c53c86de56152940568efb258444c104f01
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-22 23:58:49 +0545

    Date convert using culture info

---
+++ culturedateinfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace dateconvert
+{
+    class culturedateinfo
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo MyCultureInfo = new CultureInfo("de-DE");
+            string date1 = "22 Jan 2020";
+            DateTime datetime = DateTime.Parse(date1, MyCultureInfo);
+            Console.WriteLine("Date:"+"\t"+datetime);
+            Console.ReadLine();
+        }
+    }
+}
