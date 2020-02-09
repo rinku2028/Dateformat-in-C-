f343183e34bdb4487ff1e7102fb73f1919fe2fda
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-09 23:50:23 +0545

    Add files via upload

---
+++ numofdays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace datetime
+{
+    class numofdays
+    {
+        public int CalculateDaysInAYear(int year)
+        {
+            int days = 0;
+            for (int i = 1; i <= 12; i++)
+            {
+                days += DateTime.DaysInMonth(year, i);
+            }
+            return days;
+        }
+        public static void Main(string []args)
+        {
+            numofdays dd = new numofdays();
+            int calcdays;
+            calcdays=dd.CalculateDaysInAYear(2000);
+            Console.WriteLine("Number of days in the year 2020:" + calcdays);
+            //Output is Number of days in the year 2020: 366
+            Console.ReadLine();
+            
+        }
+
+    }
+}
