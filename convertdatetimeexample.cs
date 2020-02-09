974790c7ed76b3f038d5ea3aee6e87c10d01740a
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-09 23:55:05 +0545

    Add files via upload

---
+++ convertdatetimeexample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace datetime
+{
+    class convertdatetimeexample
+    {
+        static void Main(string []args)
+        {
+            DateTime dob = new DateTime(2020, 01, 09);
+            Console.WriteLine("Convert ToString: " + dob.ToString());
+            Console.WriteLine("Convert ToBinary: " + dob.ToBinary());
+            Console.WriteLine("Convert ToFileTime: " + dob.ToFileTime());
+            Console.WriteLine("Convert ToLocalTime: " + dob.ToLocalTime());
+            Console.WriteLine("Convert ToLongDateString: " + dob.ToLongDateString());
+            Console.WriteLine("Convert ToLongTimeString: " + dob.ToLongTimeString());
+            Console.WriteLine("Convert ToOADate: " + dob.ToOADate());
+            Console.WriteLine("Convert ToShortDateString: " + dob.ToShortDateString());
+            Console.WriteLine("Convert ToShortTimeString: " + dob.ToShortTimeString());
+            Console.WriteLine("Convert ToUniversalTime: " + dob.ToUniversalTime());
+            Console.ReadLine();
+        }
+    }
+}
